2fdedd20667148d667ba42ab66051a5118264afd
A'Yanna Rouse <ayanna.rouse2@mail.dcu.ie>
2024-03-21 13:29:44 +0000

    finally all task are done

--- bigrams.bash
+++ bigrams.bash
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-grep -v "'s$" | awk '{for(i=1;i<length($0);i++) {print substr($0, i, 2)}}'
- | sort | uniq -c | sort -nr | head -n 10 | grep -o '..$' 
+grep -v "'s$" | awk '{for(i=1;i<length($0);i++) {print substr($0, i, 2)}}'| sort | uniq -c | sort -nr | head -n 10 | grep -o '..$' 
